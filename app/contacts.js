1a78c26e8d957ef5d04a538ed01bc1267fbbf280
Jonathan Freeland <jopfre@Jonathans-MacBook-Pro.local>
2024-01-14 17:19:18 +0200

    support worker bug

--- app/contacts.js
+++ app/contacts.js
@@ -160,6 +160,7 @@ export default function Contacts() {
         ))}
       <Button
         onPress={() => {
+          setIsSupportWorker(false);
           setModalVisible(true);
         }}
         height={150}
